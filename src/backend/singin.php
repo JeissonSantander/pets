612f081a6fa8701c1050cb481e0dcf763736e56f
Jeisson <danilosantander178@gmail.com>
2024-05-03 12:11:38 -0500

    Corte 2

--- src/backend/singin.php
+++ src/backend/singin.php
@@ -1,14 +1,27 @@
+<?php 
 include('../../config/databases.php');
 
 $email = $_POST['email'];
-$pass = $_POST['passdw'];
+$passwd = $_POST['passwd'];
 $enc_pass = md5($passwd);
 
 $sql = "
-    SELECT * 
+    SELECT 
     FROM
         users2
     WHERE
         email = '$email' AND
-        password = ''
-"
+        password = '$enc_pass'
+    LIMIT 1
+";
+$result = pg_query($conn, $sql);
+$total = pg_num_rows($result);
+if($total >0){
+    //echo"login OK"
+    echo "<script>alert('User has been registered')</script>" ;
+    header("refresh:0;url=../home.php");
+}else{
+    echo"CREDENCIALES INCORRECTAS";
+}
+
+?>
--- src/backend/singup.php
+++ src/backend/singup.php
@@ -6,17 +6,32 @@
     $passwd = $_POST['passwd'];
     $enc_pass = md5($passwd);
 
-    $sql = "
+    $sql_validate_email = " SELECT * FROM users2 WHERE email= '$email'";
+    $result = pg_query($conn, $sql_validate_email);
+    $total = pg_num_rows($result);
+
+    if($total >0){
+        echo "<script>alert('Email already exists')</script>" ;
+        header("refresh:0;url=../signin.html");
+
+    }else{
+
+        $sql = "
         INSERT INTO users2 (fullname, email, password) 
             VALUES ('$fullname', '$email','$enc_pass')
-    ";
+        ";
 
-    $ans = pg_query($conn , $sql);
-    if ($ans){
-        echo "User has been created successfully";
-    }else{
-        echo "Error: " . pg_last_error();
+        $ans = pg_query($conn , $sql);
+        if ($ans){
+            echo "<script>alert('User has been registered')</script>";
+            header("refresh:0;url=../signin.html");
+        }else{
+            
+            echo "Error: " . pg_last_error();
+        }
     }
+
+   
 
     //Close connection
     pg_close($conn)
